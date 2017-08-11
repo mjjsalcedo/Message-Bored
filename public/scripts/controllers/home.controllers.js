3a7f423c0886b4a2f7c56272e0fd33dba19b28f4
Mariel Salcedo <mjjsalcedo@yahoo.com>
2017-08-10 16:16:52 -1000

    able to post a new topic and angular section done!

--- public/scripts/controllers/home.controllers.js
+++ public/scripts/controllers/home.controllers.js
@@ -5,4 +5,15 @@ angular.module('app')
     .then(function(topics) {
       $scope.topics = topics;
     });
+    $scope.newTopic = { name: ''};
+    $scope.addTopic = function() {
+      console.log('boop');
+      var newTopic = {
+        name: $scope.newTopic.name,
+        created_by: localStorage.username
+      };
+      console.log('controller', newTopic);
+      TopicService.addTopic(newTopic);
+      $scope.newTopic.name = '';
+    };
   }]);
